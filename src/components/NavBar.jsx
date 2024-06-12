8f89ad3232e0d1df7f94abace1d7c4693451df69
Aur3liaP <pic.aurelia@gmail.com>
2024-06-12 20:24:19 +0200

    finalize page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
 
     return (
       <div className='navBar'>
-        <img src="../assets/logo.png" alt="Logo Wild" className='logo'/>
+        <img src="./assets/logo.png" alt="Logo Wild" className='logo'/>
         <h1>Wild Chickens School</h1>
       </div>
     )
